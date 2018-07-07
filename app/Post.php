3dc89e97bba9804a769ce377dd071a46c181a12d
Avraham Markov <papaabramg@gmail.com>
2018-07-07 20:57:31 +0300

    models: chapter 4

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title','content'];
+
+    public function likes()
+    {
+        return $this->hasMany('App\Like');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id')->withTimestamps();
+    }
+
 }
